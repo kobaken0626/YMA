6eb96b68df8a46151e875351e6d5b9842ac33812
RX-0 <zeal23310160@fsgsl.jp>
2024-05-29 15:50:49 +0900

    第三部～完～

---
+++ 100mame1/100mame1/App.xaml.cs
@@ -0,0 +1,12 @@
+﻿namespace _100mame1
+{
+    public partial class App : Application
+    {
+        public App()
+        {
+            InitializeComponent();
+
+            MainPage = new AppShell();
+        }
+    }
+}
